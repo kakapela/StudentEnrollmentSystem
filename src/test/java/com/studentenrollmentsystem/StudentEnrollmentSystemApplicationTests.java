cbbc0357438d3997d075c8382d8093d010f3318e
kakapela <kacperkapela1@gmail.com>
2020-05-04 09:18:26 +0200

    init commit

---
+++ src/test/java/com/studentenrollmentsystem/StudentEnrollmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentenrollmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentEnrollmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
